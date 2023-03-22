60d5f9325754ecdc186d6e1fd914682c3c018135
egork <egork@DESKTOP-K108DCI>
2023-03-22 12:23:37 +0300

    Добавьте файлы проекта.

---
+++ Fitness.BL/Model/Gender.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fitness.BL.Model
+{
+    /// <summary>
+    /// Пол. 
+    /// </summary>
+    public class Gender
+    {
+        public string Name { get; }
+        public Gender(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentNullException("Пол не может быть пустым",nameof(name));
+            }
+            Name = name;
+        }
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
---
+++ Fitness.BL/Model/User.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fitness.BL.Model
+{
+    public class User
+    {/// <summary>
+    /// Пользователь.
+    /// </summary>
+        public string Name { get; }
+        public Gender Gender { get; }
+        public DateTime BirthDate { get; }
+        public double Weight { get; set; }
+        public double Heigth { get; set; }
+
+        public User(string name, Gender gender, DateTime birthDate, double weigth, double heigth)
+        {
+            #region Проверка условий ввода
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentNullException("Введите имя пользователя", nameof(name));
+            }
+            if (gender == null)
+            {
+                throw new ArgumentNullException("Введите пол", nameof(gender));
+            }
+            if (birthDate < DateTime.Parse("01.01.1900") && birthDate >= DateTime.Now) 
+            {
+                throw new ArgumentException("Введите корректную дату рождения", nameof(birthDate));
+            }
+            if (weigth <= 0)
+            {
+                throw new ArgumentException("Введите корректный ваш вес", nameof(weigth));
+            }
+            if (heigth <= 0)
+            {
+                throw new ArgumentException("Введите корректный ваш рост", nameof(heigth));
+            }
+            #endregion
+            Name = name;
+            Gender = gender;
+            BirthDate = birthDate;
+            Heigth = heigth;
+            Weight = weigth;
+        }
+        public override string ToString()
+        {
+            return Name;
+        }
+
+    }
+}
